1537b1ae154e988786b946f51637418bcf937e86
wsyyzx0 <wsyyzx0@gmail.com>
2016-10-29 07:57:50 -0400

    cities

---
+++ config/initializers/forecast.rb
@@ -0,0 +1,3 @@
+ForecastIO.configure do |configuration| 
+  configuration.api_key = '5ab4138dad746ea2e5052b361b581b36' 
+end
--- config/initializers/nominatim.rb
+++ config/initializers/nominatim.rb
@@ -1,3 +1,3 @@
 Nominatim.configure do |config|
-  config.email = 'sebastien.gadot@utbm.fr'
+  config.email = 'wsyyzx0@gmail.com'
 end
