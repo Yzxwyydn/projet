b21fc10f2efee376a566052916ecf2b331d03a9f
wsyyzx0 <wsyyzx0@gmail.com>
2016-11-26 05:24:15 -0500

    ...

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,4 @@
 module ApplicationHelper
-<<<<<<< HEAD
   def nav_link(link_text, link_path)
   class_name = current_page?(link_path) ? 'active' : ''
 
@@ -7,6 +6,4 @@ module ApplicationHelper
     link_to link_text, link_path
   end
 end
-=======
->>>>>>> 0f323978276af20bcd3de1cd076734b40f93a6a6
 end
